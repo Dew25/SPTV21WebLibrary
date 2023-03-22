d853ffe04db0fea2267f64d96e0935e0e4695fb3
none <none@ee.ee>
2023-03-22 11:31:10 +0200

    Добавили вывод читаемых книг при показе списка пользователей

--- src/java/session/HistoryFacade.java
+++ src/java/session/HistoryFacade.java
@@ -2,7 +2,9 @@
 
 package session;
 
+import entity.Book;
 import entity.History;
+import entity.Reader;
 import java.util.ArrayList;
 import java.util.List;
 import javax.ejb.Stateless;
@@ -28,6 +30,16 @@ public class HistoryFacade extends AbstractFacade<History> {
     public List<History> getHistoriesWithTakedBooks() {
         try {
             return em.createQuery("SELECT h FROM History h WHERE h.returnBook = null")
+                    .getResultList();
+        } catch (Exception e) {
+            return new ArrayList<>();
+        }
+    }
+
+    public List<Book> getReadingBook(Reader reader) {
+        try {
+            return em.createQuery("SELECT h.book FROM History h WHERE h.returnBook = null AND h.reader = :reader")
+                    .setParameter("reader", reader)
                     .getResultList();
         } catch (Exception e) {
             return new ArrayList<>();
